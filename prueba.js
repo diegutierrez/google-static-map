a3cc21152825ccb6fbbdf692d9dd165e9781846d
Diego Gutierrez <ibox.org@gmail.com>
2017-11-07 11:47:33 -0300

    test

--- prueba.js
+++ prueba.js
@@ -1,6 +1,14 @@
 var fs = require('fs');
 var gm = require('google-static-map').set('google-console-api-key');
 
+var publicConfig = {
+    proxy: 'http://e378395:Hmmi1468@ansesproxy.anses.gov.ar:80' // optional, set a proxy for HTTP requests
+};
+
+var stream = gm(publicConfig).address('Apple Store, 5th Avenue, New York').staticMap().done();
+stream.pipe(fs.createWriteStream('testt.png'));
+
+/*
 var stream = gm()
     .zoom( 16 )
     .resolution( '962x250' )
@@ -8,7 +16,7 @@ var stream = gm()
     .staticMap()
     .done();
 
-stream.pipe(fs.createWriteStream('test.jpg'));
+stream.pipe(fs.createWriteStream('test.jpg'));*/
 
 //http://maps.googleapis.com/maps/api/staticmap?
 // center=-34.638493,-58.369617&
